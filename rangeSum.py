b86cfb5be8a0b7e26bfa13f9661ea36eb16e6c2e
Donghyeon <parkdonghyeon1@gamil.com>
2023-06-07 16:13:27 -0500

    fourth commit

--- rangeSum.py
+++ rangeSum.py
@@ -16,3 +16,6 @@ class NumArray(object):
         #This function takes the start and end point of list, then returns the sum of the whole integers.
 
         return sum(self.nums[left,right+1])
+    ################
+
+    ##########
